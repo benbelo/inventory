1fb3e85a262a02e4c7f76a3ad4936562e865c9d3
benbelo <benjamin.belot@proton.me>
2025-09-10 15:24:46 +0200

    modifs

--- list-rudder-servers.sh
+++ list-rudder-servers.sh
@@ -7,21 +7,19 @@ filteredserverslist=/tmp/filteredserverslist
 norudderservers=/usr/local/sbin/norudderservers
 results=/tmp/results
 
-#Listing of servers configured on Rudder
+# On liste les serveurs dans la cli de rudder
 ssh root@be-rudder "rudder-cli node list --skip-verify | grep hostname | awk '{print $2}' | cut -d '\"' -f4 | cut -d '.' -f1" > $listrudder
-####################################
 
-#Listing of active VMs and containers on Proxmox's clusters
+# On liste les CT et VM des clusters proxmox
 ssh burp@galaxie5 'sudo pvesh get /cluster/resources -type vm --output-format yaml | grep -B4 running | egrep -i 'name' | cut -d " " -f4 | sort' > $allserverslist
 ssh burp@galaxie1-po 'sudo pvesh get /cluster/resources -type vm --output-format yaml | grep -B4 running | egrep -i 'name' | cut -d " " -f4 | sort' >> $allserverslist
 
-#Deletion of servers intentionally not backed up by burp
+# Suppression des serveurs non sauvegardés par burp
 grep -vw -f $norudderservers $allserverslist > $filteredserverslist
 
-#Listing of known and unconfigured servers on burp
+# Liste des serveurs connus et non sauve dans burp
 grep -vw -f $listrudder $filteredserverslist > $results
 
-#Sending results by email
 unbackedupservers=$(cat $results)
 if [ -s $results ]
 then
@@ -30,5 +28,5 @@ then
             cat $results | /usr/bin/mail -s "SUCCESS : All known servers are configured in Rudder" system@esiee.fr
         fi
 
-        #Deletion of temporary files
+        # delete des fichiers temp
         rm $listburp $allserverslist $filteredserverslist $results
